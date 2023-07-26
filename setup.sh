e7abda707117b07a47448643e5047258356e606b
Matteo Lustrissimi <113957454+MatteoLustrissimi@users.noreply.github.com>
2023-07-26 11:56:55 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 grep -q bullseye /etc/os-release && {
+  apt update && apt upgrade -y
+  apt --purge autoremove -y
   sed 's/bullseye/bookworm/g' -i /etc/apt/sources.list
-  apt update
+  apt update && apt upgrade --without-new-pkgs
+  apt full-upgrade -y
+  sed 's/eth/enX/g' -i /etc/network/interfaces
 }
+echo DONE
